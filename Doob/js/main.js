379a4f6cd8d83a75b339cd277989368e3f0d34ee
Artem <dumzday777@gmail.com>
2020-06-24 11:04:30 +0300

    1

---
+++ Doob/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    const mainNav = document.querySelector('.main-nav');
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+    hamburgerMenu.addEventListener('click', function() {
+        mainNav.classList.toggle('open');
+    });
+    $(document).mouseup(function(e) { // событие клика по веб-документу
+        var div = $(".main-nav"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            &&
+            div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.removeClass("open"); // скрываем его
+        }
+    });
+});
